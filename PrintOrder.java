693a1b8c65b5bd54b35c8e19bbae136a9e6f93ce
Mattia Avanzi <m8.avanzi@gmail.com>
2019-05-21 23:22:27 +0200

    Risolto tutto ciao

--- PrintOrder.java
+++ PrintOrder.java
@@ -1,9 +1,9 @@
+import javax.xml.bind.DatatypeConverter;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import java.math.BigDecimal;
-import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
@@ -43,11 +43,10 @@ public class PrintOrder {
 	 * The statement to list the last 15 orders done by a customer
 	 */
 	private static final String LIST_ORDERS = "SELECT o.order_id, o.order_timestamp, o.total_price, m.type FROM Make AS m "
-			+ "SELECT o.order_id, o.order_timestamp, o.total_price, m.type FROM Make AS m "
-			+ "INNER JOIN Orders AS o ON m.order_id = o.order_id " 
-			+ "WHERE customer_email = ? " 
-			+ "ORDER BY order_id DESC " 
-			+ "LIMIT 15; "; 
+			+ "INNER JOIN Orders AS o ON m.order_id = o.order_id "
+			+ "WHERE customer_email = ? "
+			+ "ORDER BY order_id DESC "
+			+ "LIMIT 15; ";
 
 	/**
 	 * The statement to list the details of an order
@@ -80,9 +79,9 @@ public class PrintOrder {
 	private static final String EMAIL_CHECK = "SELECT * FROM End_User WHERE email = ?;";
 
 	private static final String SELECT_PSW = "SELECT password FROM End_User WHERE email = ?;";
-	
+
 	private static final String ROLE = "SELECT role FROM End_User WHERE email = ?;";
- 	
+
 	/**
 	 * The connection to the database
 	 */
@@ -106,7 +105,7 @@ public class PrintOrder {
 	private String password;
 
 	/**
-	 * 
+	 *
 	 * @param user_email
 	 */
 	private void printOrders(String user_email) throws SQLException {
@@ -117,17 +116,16 @@ public class PrintOrder {
 			pstmt = con.prepareStatement(LIST_ORDERS);
 			pstmt.setString(1, user_email);
 			rs = pstmt.executeQuery();
-			
+
 			System.out.println("Order ID\tOrder Timestamp\tTotal Price\tPayment Method");
+			
 			while (rs.next()) {
-				System.out.printf("%d\t%t\t%s\t%s", rs.getString("order_id"),
-						rs.getString("order_timestamp"), rs.getString("total_price"), rs.getString("type"));
+				System.out.printf("%s\t%s\t%s\t%s\n", rs.getString("order_id"), rs.getString("order_timestamp"), rs.getString("total_price"), rs.getString("type"));
 			}
 		} finally{
 			if (rs != null) {
 				rs.close();
 			}
-
 			if (pstmt != null) {
 				pstmt.close();
 			}
@@ -135,7 +133,7 @@ public class PrintOrder {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param order_id
 	 */
 	private void printOrderDetail(String order_id) throws SQLException {
@@ -146,10 +144,10 @@ public class PrintOrder {
 			pstmt = con.prepareStatement(LIST_ORDER_DETAIL);
 			pstmt.setString(1, order_id);
 			rs = pstmt.executeQuery();
-			
+
 			System.out.println("Order ID\tProduct Name\tProduct Quantity\tUnit Price\tProducer");
 			while (rs.next()) {
-				System.out.printf("%d\t%s\t%s\t%s", rs.getString("order_id"),
+				System.out.printf("%s\t%s\t%s\t%s\n", rs.getString("order_id"),
 						rs.getString("name"), rs.getString("quantity"), rs.getString("Unit Price"), rs.getString("business_name"));
 			}
 		} finally{
@@ -164,7 +162,7 @@ public class PrintOrder {
 	}
 
 	/**
-	 * 
+	 *
 	 */
 	private void printEvents(String Region) throws SQLException {
 
@@ -175,10 +173,10 @@ public class PrintOrder {
 			pstmt = con.prepareStatement(LIST_EVENTS);
 			pstmt.setString(1, Region);
 			rs = pstmt.executeQuery();
-			
+
 			System.out.println("Event Name\tDescription\tStart Date\tEnd Date\tLocation\tRegion");
 			while (rs.next()) {
-				System.out.printf("%s\t%s\t%t\t%t\t%s\t%s\t%s", rs.getString("name"),
+				System.out.printf("%s\t%s\t%t\t%t\t%s\t%s\t%s\n", rs.getString("name"),
 						rs.getString("description"), rs.getString("start_date"), rs.getString("end_date"), rs.getString("location"), rs.getString("region"));
 			}
 		} finally{
@@ -193,7 +191,7 @@ public class PrintOrder {
 	}
 
 	/**
-	 * 
+	 *
 	 */
 	private void logout() {
 		email = null;
@@ -205,7 +203,7 @@ public class PrintOrder {
 
 
 	/**
-	 * 
+	 *
 	 * @param user_psw
 	 * @return true if the insert password 
 	 * @throws SQLException
@@ -214,37 +212,29 @@ public class PrintOrder {
 	 */
 
 	private Boolean checkPassword(String user_psw) throws SQLException, IllegalArgumentException, IllegalStateException , NoSuchAlgorithmException{
-		
+
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		String real_psw ="";
-		
+
 		try {
 			pstmt = con.prepareStatement(SELECT_PSW);
 			pstmt.setString(1, email);
 			rs = pstmt.executeQuery();
 			if(rs.next()){
 				real_psw = rs.getString("password");
-			} 
+			}
 			else {
 				throw new IllegalStateException();
-			}			
+			}
 			if(user_psw == null || !(user_psw instanceof String) || user_psw.length() == 0){
 				throw new IllegalArgumentException("Incorrect argument passed to the function");
 			}
-			/*
-			byte[] psw_bytes = user_psw.getBytes(StandardCharsets.UTF_8);
-			MessageDigest md = MessageDigest.getInstance("MD5");
-			byte[] psw_digested = md.digest(psw_bytes);
-			String psw_dig = new String(psw_digested);
-			*/
 
 			MessageDigest md = MessageDigest.getInstance("MD5");
-		    md.update(user_psw.getBytes());
-		    byte[] digest = md.digest();
-		    String myHash = DatatypeConverter.printHexBinary(digest).toUpperCase();
-		         
-		   //assertThat(psw_dig.equals(real_psw)).isTrue();
+			md.update(user_psw.getBytes());
+			byte[] digest = md.digest();
+			String myHash = DatatypeConverter.printHexBinary(digest).toLowerCase();
 
 			if(myHash.equals(real_psw) ){
 				return true;
@@ -262,14 +252,14 @@ public class PrintOrder {
 			}
 		}
 	}
-	
+
 	/**
-	 * 
-	 * @param email user email to check
+	 *
+	 * @param user_email user email to check
 	 * @return true if user is registered, false otherwise
 	 * @throws SQLException
 	 */
-		
+
 	private Boolean checkIfUserExists(String user_email) throws SQLException {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -291,7 +281,7 @@ public class PrintOrder {
 	}
 
 	/**
-	 * 
+	 *
 	 * @param user_email
 	 * @return
 	 * @throws SQLException
@@ -301,7 +291,7 @@ public class PrintOrder {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		String user_role = "";
-		
+
 		try {
 			pstmt = con.prepareStatement(ROLE);
 			pstmt.setString(1,user_email);
@@ -309,8 +299,9 @@ public class PrintOrder {
 
 			if(rs.next()){
 				user_role = rs.getString("role");
-			} 
-			if(user_role == "Customer"){
+			}
+
+			if(user_role.equals("Customer")){
 				return true;
 			}
 			else{
@@ -324,14 +315,14 @@ public class PrintOrder {
 			if (pstmt != null) {
 				pstmt.close();
 			}
-		}		
+		}
 	}
 
 	private Boolean is_Producer(String user_email) throws SQLException {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		String user_role = "";
-		
+
 		try {
 			pstmt = con.prepareStatement(ROLE);
 			pstmt.setString(1,user_email);
@@ -339,7 +330,7 @@ public class PrintOrder {
 
 			if(rs.next()){
 				user_role = rs.getString("role");
-			} 
+			}
 			if(user_role == "Producer"){
 				return true;
 			}
@@ -354,7 +345,7 @@ public class PrintOrder {
 			if (pstmt != null) {
 				pstmt.close();
 			}
-		}		
+		}
 	}
 	/**
 	 * Run the whole simulation process
@@ -368,16 +359,16 @@ public class PrintOrder {
 
 			//Login
 
-			/** 
+			/**
 			 * Chiedere cosa vuole visualizzare l'utente tra:
 			 * (Se Customer)
 			 * - Mostrare gli ordini fatti
 			 * - Mostrare un ordine in dettagli
 			 * - Mostrare gli eventi futuri in una regione
-			 * 
+			 *
 			 * (Se Producer o Regional Manager)
 			 * - Visualizzare le statistice di un producer
-			*/
+			 */
 
 			//Logout
 
@@ -390,7 +381,7 @@ public class PrintOrder {
 				System.out.printf("Please insert a registered email.\n");
 				email = scan.nextLine();
 			}
-		
+
 			System.out.printf("Please insert your password.\n");
 			String user_password = scan.nextLine();
 
@@ -399,37 +390,33 @@ public class PrintOrder {
 				user_password = scan.nextLine();
 			}
 
-			System.out.println(user_password);
-			//User logged in
-			
-
 			if(is_Customer(email)){
-					Boolean goodCommand = false;
-					while(!goodCommand) {
-					System.out.printf("Please what do you want to do?");
-					System.out.printf("Digit 1 for visualize Order History");
-					System.out.printf("Digit 2 for visualize a Order Detail");
-					System.out.printf("Digit 3 for visualize a Future Events");
-					System.out.printf("Digit q to logout");
+				Boolean goodCommand = false;
+				while(!goodCommand) {
+					System.out.printf("Please what do you want to do?\n");
+					System.out.printf("Digit 1 for visualize Order History\n");
+					System.out.printf("Digit 2 for visualize a Order Detail\n");
+					System.out.printf("Digit 3 for visualize a Future Events\n");
+					System.out.printf("Digit q to logout\n");
 					String choice = scan.nextLine();
-					if(choice == "1") {
+					if(choice.equals("1")) {
 						printOrders(email);
-					} else if (choice == "2") {
+					} else if (choice.equals("2")) {
 						System.out.printf("Insert your order ID");
 						String order_id = scan.nextLine();
 						printOrderDetail(order_id);
-					} else if (choice == "3") {
+					} else if (choice.equals("3")) {
 						System.out.printf("Insert the Region");
 						String region = scan.nextLine();
 						printEvents(region);
-					} else if (choice == "q") {
+					} else if (choice.equals("q")) {
 						logout();
 						goodCommand = true;
-					}	
+					}
 				}
 
 			} else if(is_Producer(email)) {
-				System.out.println("Xe un produtore!"); // TODO: remove
+				System.out.println("Xe un produtore!"); //TODO: remove
 			}
 
 		} catch (SQLException e) {
@@ -470,7 +457,7 @@ public class PrintOrder {
 
 		try {
 			long time = -System.currentTimeMillis();
-			con = DriverManager.getConnection(DATABASE, USER, PASSWORD);								
+			con = DriverManager.getConnection(DATABASE, USER, PASSWORD);
 			time += System.currentTimeMillis();
 
 			System.out.printf("Connection to database %s successfully established in %,d milliseconds.%n",

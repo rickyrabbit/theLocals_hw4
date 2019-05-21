a5bf8c0ad3ec140a412f77cb642f7cc27d213d4a
Mattia Avanzi <ava8c@hotmail.it>
2019-05-21 10:56:54 +0200

    Modificato ordine insert population

--- db_population.sql
+++ db_population.sql
@@ -9,7 +9,7 @@ INSERT INTO Region(name) VALUES
 INSERT INTO Role(role) VALUES  -- mi restituiva ERRORE:  la colonna "role_type" della relazione "role" non esiste
 ('Restaurateur'), ('Regional Manager'), ('Event Organizer'), ('Customer'), ('Producer');
 
-INSERT INTO TABLE Sales_Channel(type) VALUES
+INSERT INTO Sales_Channel(type) VALUES
 ('Pay In store'), ('Cash On delivery');
 
 INSERT INTO Status(status) VALUES
@@ -29,8 +29,30 @@ INSERT INTO Category(category_id, name, description) VALUES
 ('A11', 'Birra','Produzione, lavorazione e vendita di: BIRRA '),
 ('A12', 'Aceti','Produzione, lavorazione e vendita di: ACETI');
 
+INSERT INTO Product(name, general_description, category_id) VALUES
+('Sopressa Vicentina D.O.P.', 'La Soppressa Vicentina è ottenuta dalla lavorazione di cosce, coppa, spalla, pancetta, grasso di gola e lombo di maiale.', 'A1'),
+('Coppa di Testa di Este', 'La coppa di testa viene prodotta da tempo immemorabile dai contadini dell’estense, nel periodo invernale, immediatamente dopo la macellazione del maiale.', 'A1'),
+('Gallina Padovana', 'Razza riconosciuta come "pura" da uno standard nazionale redatto dalla Federazione Italiana delle razze avicole.', 'A2'),
+('Coniglio Veneto', 'Anticamente l’allevamento del coniglio costituiva la forma di reddito integrativo per le famiglie della mezzadria veneta.', 'A2'),
+('Miele delle Dolomiti Bellunesi D.O.P.', 'Miele prodotto a partire dal nettare dei fiori del territorio montano bellunese, dall’ecotipo locale di “Apis mellifera.”', 'A3'),
+('Confettura di Mirtillo e Mela della Altopiano di Asiago', 'Esistono numerosi tipi di marmellate e confetture tipiche delle diverse zone del Veneto dovute alla ricca produzione di frutta.', 'A4'),
+('Confettura di Ciliegia di Marostica I.G.P.', 'Esistono numerosi tipi di marmellate e confetture tipiche delle diverse zone del Veneto dovute alla ricca produzione di frutta.', 'A4'),
+('Panada Veneta', 'Pancotto aromatizzato alla cannella.', 'A5'),
+('Sopa Coada', 'Zuppa gratinata composta di strati di pane raffermo e piccione', 'A5'),
+('Olio Extravergine Di Oliva D.O.P.', 'Olio tipico dei Colli Euganei.', 'A6'),
+('Asiago D.O.P.', 'Formaggio tipico delle zone limitrofe all''Altopiano di Asiago.', 'A7'),
+('Chiocciole D.O.P. Vicentine', 'Chiocciole tipiche della pianura vicino a Vicenza.', 'A8'),
+('Calamari Veneziani', 'Pesce tipico della laguna di Venezia.', 'A9'),
+('Bigoli Veneti', 'Ottima pasta da abbinare con il ragù d''anatra.', 'A10'),
+('Birra di Rovigo D.O.P.', 'La birra artigianale è un prodotto non pastorizzato e non filtrato.', 'A11'),
+('Aceto di Vino Euganeo', 'Aceto tipico dei Colli Euganei.', 'A12');
 
---:TODO psw must be in md5
+INSERT INTO Restaurant(name, email, location, description, images, telephone_number, region_name) VALUES
+('Le calandre', 'lecalandre@gmail.com', 'Padova', 'Traditional Italian restaurant', NULL, '0490000', 'Veneto'),
+('Da Orazio', 'daorazio@gmail.com', 'Treviso', 'Seafood restaurant', NULL, '04221000', 'Veneto'),
+('Da Luisa', 'daluisa@gmail.com', 'Venezia', 'Trattoria', NULL, '346013406', 'Veneto'),
+('Da Pino', 'dapino@gmail.com', 'Verona', 'Pizzeria', NULL, '346713406', 'Veneto');
+
 INSERT INTO End_User(email, password, first_name, last_name, validated, organization, role, tax_code) VALUES
 ('Giovanni.Aquila@gmail.com', md5('12345'), 'Giovanni', 'Aquila', NULL, NULL, 'Restaurateur', 'QLAGNN80P11G273B'),
 ('Orazio.Gatti@gmail.com', md5('12345'), 'Orazio', 'Gatti', NULL, NULL, 'Restaurateur', 'GTTRZO75P08D612O'),
@@ -53,20 +75,6 @@ INSERT INTO End_User(email, password, first_name, last_name, validated, organiza
 ('Gualberto.Alescio@gmail.com', md5('12345'), 'Gualberto', 'Alescio', TRUE, NULL, 'Producer', 'LSCGBR80P11H501P'),
 ('Beatrice.Altoviti@gmail.com', md5('12345'), 'Beatrice', 'Altoviti', TRUE, NULL, 'Producer', 'LTVBRC80M48G224W');
 
-INSERT INTO Producer(email, pec, activity_description , location, telephone_number , business_name , vat_number text NOT NULL, region_name ) VALUES
-('Tatiana.Agnelli@gmail.com', 'Tatiana.Agnelli@legalmail.it', 'Allevamento di maiali e produzione propria di salumi', 'Conegliano', '3923085842', 'Le carni di Tatiana', '01906530983', 'Veneto'),
-('Angelo.Antonini@gmail.com', 'Angelo.Antonini@legalmail.it', 'Produzione di olio dei Colli Euganei', 'Cinto Euganeo', '0429634030', 'Sapori dei Colli', '01835500940', 'Veneto'),
-('Gualberto.Alescio@gmail.com', 'Gualberto.Alescio@legalmail.it', 'Piccolo allevamento di conigli', 'Padova', '0496588741', 'Allevamento Alescio', '02976538413', 'Veneto'),
-('Beatrice.Altoviti@gmail.com', 'Beatrice.Altoviti@legalmail.it', 'Produzione di confetture', 'Asiago', '0424461475', 'Le confetture di Bea', '01984568450', 'Veneto');
-
-INSERT INTO Belong1(email, category_id ) VALUES
-('Tatiana.Agnelli@gmail.com','A1')
-('Angelo.Antonini@gmail.com','A6')
-('Gualberto.Alescio@gmail.com','A2')
-('Beatrice.Altoviti@gmail.com','A4');
-
-
--- FARE GLI ORDINI PRIMA DEL 5 maggio
 INSERT INTO Orders(order_id, total_price, order_timestamp, order_status) VALUES
 ('20.20', '2019-03-24 14:13:25+02', 'Canceled')
 ('2.50', '2019-03-29 12:05:09+02', 'Canceled')
@@ -77,8 +85,7 @@ INSERT INTO Orders(order_id, total_price, order_timestamp, order_status) VALUES
 ('8.05', '2019-04-22 12:15:00+02', 'Completed')
 ('12.05', '2019-04-27 20:37:55+02', 'Completed')
 ('12.35', '2019-05-01 19:18:57+02', 'Completed')
-('14.20', '2019-05-03 13:33:47+02', 'Reserved');
-
+('14.20', '2019-05-03 13:33:47+02', 'Completed');
 
 INSERT INTO Contain(order_id, product_code, quantity, price) VALUES
 (1, 1, 2, '12.05'),
@@ -96,31 +103,11 @@ INSERT INTO Contain(order_id, product_code, quantity, price) VALUES
 (9, 1, 2, '12.35'),
 (10, 10, 1, '14.20');
 
-INSERT INTO Product(name, general_description, category_id) VALUES
-('Sopressa Vicentina D.O.P.', 'La Soppressa Vicentina è ottenuta dalla lavorazione di cosce, coppa, spalla, pancetta, grasso di gola e lombo di maiale.', 'A1'),
-('Coppa di Testa di Este', 'La coppa di testa viene prodotta da tempo immemorabile dai contadini dell’estense, nel periodo invernale, immediatamente dopo la macellazione del maiale.', 'A1'),
-('Gallina Padovana', 'Razza riconosciuta come "pura" da uno standard nazionale redatto dalla Federazione Italiana delle razze avicole.', 'A2'),
-('Coniglio Veneto', 'Anticamente l’allevamento del coniglio costituiva la forma di reddito integrativo per le famiglie della mezzadria veneta.', 'A2'),
-('Miele delle Dolomiti Bellunesi D.O.P.', 'Miele prodotto a partire dal nettare dei fiori del territorio montano bellunese, dall’ecotipo locale di “Apis mellifera.”', 'A3'),
-('Confettura di Mirtillo e Mela della Altopiano di Asiago', 'Esistono numerosi tipi di marmellate e confetture tipiche delle diverse zone del Veneto dovute alla ricca produzione di frutta.', 'A4'),
-('Confettura di Ciliegia di Marostica I.G.P.', 'Esistono numerosi tipi di marmellate e confetture tipiche delle diverse zone del Veneto dovute alla ricca produzione di frutta.', 'A4'),
-('Panada Veneta', 'Pancotto aromatizzato alla cannella.', 'A5'),
-('Sopa Coada', 'Zuppa gratinata composta di strati di pane raffermo e piccione', 'A5'),
-('Olio Extravergine Di Oliva D.O.P.', 'Olio tipico dei Colli Euganei.', 'A6'),
-('Asiago D.O.P.', 'Formaggio tipico delle zone limitrofe all''Altopiano di Asiago.', 'A7'),
-('Chiocciole D.O.P. Vicentine', 'Chiocciole tipiche della pianura vicino a Vicenza.', 'A8'),
-('Calamari Veneziani', 'Pesce tipico della laguna di Venezia.', 'A9'),
-('Bigoli Veneti', 'Ottima pasta da abbinare con il ragù d''anatra.', 'A10'),
-('Birra di Rovigo D.O.P.', 'La birra artigianale è un prodotto non pastorizzato e non filtrato.', 'A11'),
-('Aceto di Vino Euganeo', 'Aceto tipico dei Colli Euganei.', 'A12');
-
-INSERT INTO Sell(email , product_code , price, stock, image,  producer_description) VALUES
-('Tatiana.Agnelli@gmail.com', 1, '12.05', 7, NULL, 'Soppressa Vicentina D.O.P di coppa e spalla aromatizata con rosmarino.800gr '),
-('Tatiana.Agnelli@gmail.com', 2, '8.10', 10, NULL, 'Cappa di testa tradizionale estense aromatizzata al timo.900gr'),
-('Angelo.Antonini@gmail.com', 10, '14.20', 5, NULL, 'Olio dei Colli Euganei spremuto a freddo,dal retrogusto piccante.1L'),
-('Gualberto.Alescio@gmail.com', 4, '12.30', 37, NULL, 'Coniglio intero allevato all''aperto.1800gr'),
-('Beatrice.Altoviti@gmail.com', 6, '3.60', 40, NULL, 'Confettura di Mirtillo e Mela di coltivazioni secolari presenti nell'' Altopiano.600gr'),
-('Beatrice.Altoviti@gmail.com', 7, '2.50', 38, NULL, 'Deliziosa confettura della rinomata Ciliegia di Marostica,famosa per il suo gusto caramelloso.550gr');
+INSERT INTO Producer(email, pec, activity_description , location, telephone_number , business_name , vat_number text NOT NULL, region_name ) VALUES
+('Tatiana.Agnelli@gmail.com', 'Tatiana.Agnelli@legalmail.it', 'Allevamento di maiali e produzione propria di salumi', 'Conegliano', '3923085842', 'Le carni di Tatiana', '01906530983', 'Veneto'),
+('Angelo.Antonini@gmail.com', 'Angelo.Antonini@legalmail.it', 'Produzione di olio dei Colli Euganei', 'Cinto Euganeo', '0429634030', 'Sapori dei Colli', '01835500940', 'Veneto'),
+('Gualberto.Alescio@gmail.com', 'Gualberto.Alescio@legalmail.it', 'Piccolo allevamento di conigli', 'Padova', '0496588741', 'Allevamento Alescio', '02976538413', 'Veneto'),
+('Beatrice.Altoviti@gmail.com', 'Beatrice.Altoviti@legalmail.it', 'Produzione di confetture', 'Asiago', '0424461475', 'Le confetture di Bea', '01984568450', 'Veneto');
 
 INSERT INTO Review(email, product_code, score, content, review_timestamp) VALUES
 ('Evelina.Piazza@gmail.com', 1, 5, 'Sopressa di ottima fattura, con un buon rapporto qualità/prezzo.', '2019-05-10 11:23:54+02'),
@@ -130,17 +117,18 @@ INSERT INTO Review(email, product_code, score, content, review_timestamp) VALUES
 ('Nicola.Abelli@gmail.com', 11, 5, 'Asiago perfetto.', '2019-05-18 10:23:54+02'),
 ('Nicola.Abelli@gmail.com', 12, 1, 'Chiocciole arrivare troppo vecchie, le ho buttate.', '2019-05-19 09:29:54+02');
 
-INSERT INTO Restaurant(name, email, location, description, images, telephone_number, region_name) VALUES
-('Le calandre', 'lecalandre@gmail.com', 'Padova', 'Traditional Italian restaurant', NULL, '0490000', 'Veneto'),
-('Da Orazio', 'daorazio@gmail.com', 'Treviso', 'Seafood restaurant', NULL, '04221000', 'Veneto'),
-('Da Luisa', 'daluisa@gmail.com', 'Venezia', 'Trattoria', NULL, '346013406', 'Veneto'),
-('Da Pino', 'dapino@gmail.com', 'Verona', 'Pizzeria', NULL, '346713406', 'Veneto');
+INSERT INTO Event(name, location, date_range, description, email, region_name) VALUES
+('Festa di Primavera', 'Via Casoni, 31057 Casale sul Sile (TV)', '[2019-03-20,2019-03-23]', 'Musica, vino e artigianato, tutti i giorni dalle 20 alle 23', 'Antonio.Como@gmail.com', 'Veneto'),
+('In Vino Veritas', 'Prato della Valle, 35123 Padova (PD)', '[2019-09-15,2019-09-20]', 'Gustate i vini dei colli, tutti i giorni dalle 10 alle 23', 'Settimo.Albanesi@gmail.com', 'Veneto'),
+('Orto in Tavola', 'Piazza IV Novembre, 06100 Perugia (PG)', '[2019-06-10,2019-06-20]', 'Le migliori produzioni locali in piazza, tutti i giorni dalle 10 alle 22', 'Fioralba.Murgia@gmail.com', 'Umbria'),
+('Festa della tagliata', 'Piazza della Biade, 36100 Vicenza (VI)', '[2019-07-01,2019-07-05]', 'Carne di qualità tutte le sere dalle 19 alle 00', 'Antonio.Como@gmail.com', 'Veneto'),
+('Festa del Pane', 'Piazza del Duomo, 56126 Pisa (PI)', '[2019-06-25,2019-06-30]', 'Cibo e musica tutte le sere dalle 19 alle 23', 'Michelina.Corti@gmail.com', 'Toscana');
 
-INSERT INTO Own(restaurant_id, email) VALUES
-(1, 'Giovanni.Aquila@gmail.com'),
-(2, 'Orazio.Gatti@gmail.com'),
-(3, 'Luisa.Ferrara@gmail.com'),
-(4, 'Enzo.Tumicelli@gmail.com');
+INSERT INTO Manage(email, region_name) VALUES
+('Salvatore.Aloia@gmail.com', 'Veneto')
+('Ginevra.Barsotti@gmail.com', 'Piemonte')
+('Uberto.Innocenti@gmail.com', 'Trentino-Alto Adige')
+('Ambrogio.Sparacello@gmail.com', 'Friuli-Venezia Giulia');
 
 INSERT INTO Offer(restaurant_id, product_code) VALUES
 (1, 3),
@@ -150,33 +138,11 @@ INSERT INTO Offer(restaurant_id, product_code) VALUES
 (3, 1),
 (4, 10);
 
-INSERT INTO Event(name, location, date_range, description, email, region_name) VALUES
-('Festa di Primavera', 'Via Casoni, 31057 Casale sul Sile (TV)', '[2019-03-20,2019-03-23]', 'Musica, vino e artigianato, tutti i giorni dalle 20 alle 23', 'Antonio.Como@gmail.com', 'Veneto'),
-('In Vino Veritas', 'Prato della Valle, 35123 Padova (PD)', '[2019-09-15,2019-09-20]', 'Gustate i vini dei colli, tutti i giorni dalle 10 alle 23', 'Settimo.Albanesi@gmail.com', 'Veneto'),
-('Orto in Tavola', 'Piazza IV Novembre, 06100 Perugia (PG)', '[2019-06-10,2019-06-20]', 'Le migliori produzioni locali in piazza, tutti i giorni dalle 10 alle 22', 'Fioralba.Murgia@gmail.com', 'Umbria'),
-('Festa della tagliata', 'Piazza della Biade, 36100 Vicenza (VI)', '[2019-07-01,2019-07-05]', 'Carne di qualità tutte le sere dalle 19 alle 00', 'Antonio.Como@gmail.com', 'Veneto'),
-('Festa del Pane', 'Piazza del Duomo, 56126 Pisa (PI)', '[2019-06-25,2019-06-30]', 'Cibo e musica tutte le sere dalle 19 alle 23', 'Michelina.Corti@gmail.com', 'Toscana');
-
-INSERT INTO Promote(email, product_code, event_id) VALUES
-('Beatrice.Altoviti@gmail.com', 6, 1),
-('Beatrice.Altoviti@gmail.com', 6, 1),
-('Beatrice.Altoviti@gmail.com', 7, 1),
-('Angelo.Antonini@gmail.com', 10, 2),
-('Gualberto.Alescio@gmail.com', 4, 2);
-
-INSERT INTO Sale_Through(type, email) VALUES
-('Pay In store', 'Tatiana.Agnelli@gmail.com'),
-('Cash On delivery', 'Tatiana.Agnelli@gmail.com'),
-('Pay In store', 'Angelo.Antonini@gmail.com'),
-('Cash On delivery', 'Angelo.Antonini@gmail.com'),
-('Pay In store', 'Gualberto.Alescio@gmail.com'),
-('Pay In store', 'Beatrice.Altoviti@gmail.com');
-
-INSERT INTO Manage(email, region_name) VALUES
-('Salvatore.Aloia@gmail.com', 'Veneto')
-('Ginevra.Barsotti@gmail.com', 'Piemonte')
-('Uberto.Innocenti@gmail.com', 'Trentino-Alto Adige')
-('Ambrogio.Sparacello@gmail.com', 'Friuli-Venezia Giulia');
+INSERT INTO Own(restaurant_id, email) VALUES
+(1, 'Giovanni.Aquila@gmail.com'),
+(2, 'Orazio.Gatti@gmail.com'),
+(3, 'Luisa.Ferrara@gmail.com'),
+(4, 'Enzo.Tumicelli@gmail.com');
 
 INSERT INTO Make(order_id, type, customer_email, producer_email) VALUES
 (1, 'Cash On delivery', 'Evelina.Piazza@gmail.com', 'Angelo.Antonini@gmail.com'),
@@ -189,3 +155,32 @@ INSERT INTO Make(order_id, type, customer_email, producer_email) VALUES
 (8, 'Cash On delivery', 'Gualtiero.Aldebrandi@gmail.com', 'Tatiana.Agnelli@gmail.com'),
 (9, 'Pay In store', 'Evelina.Piazza@gmail.com', 'Tatiana.Agnelli@gmail.com'),
 (10, 'Cash On delivery', 'Gianpaolo.Abano@gmail.com', 'Angelo.Antonini@gmail.com');
+
+INSERT INTO Sell(email , product_code , price, stock, image,  producer_description) VALUES
+('Tatiana.Agnelli@gmail.com', 1, '12.05', 7, NULL, 'Soppressa Vicentina D.O.P di coppa e spalla aromatizata con rosmarino.800gr '),
+('Tatiana.Agnelli@gmail.com', 2, '8.10', 10, NULL, 'Cappa di testa tradizionale estense aromatizzata al timo.900gr'),
+('Angelo.Antonini@gmail.com', 10, '14.20', 5, NULL, 'Olio dei Colli Euganei spremuto a freddo,dal retrogusto piccante.1L'),
+('Gualberto.Alescio@gmail.com', 4, '12.30', 37, NULL, 'Coniglio intero allevato all''aperto.1800gr'),
+('Beatrice.Altoviti@gmail.com', 6, '3.60', 40, NULL, 'Confettura di Mirtillo e Mela di coltivazioni secolari presenti nell'' Altopiano.600gr'),
+('Beatrice.Altoviti@gmail.com', 7, '2.50', 38, NULL, 'Deliziosa confettura della rinomata Ciliegia di Marostica,famosa per il suo gusto caramelloso.550gr');
+
+INSERT INTO Promote(email, product_code, event_id) VALUES
+('Beatrice.Altoviti@gmail.com', 6, 1),
+('Beatrice.Altoviti@gmail.com', 6, 1),
+('Beatrice.Altoviti@gmail.com', 7, 1),
+('Angelo.Antonini@gmail.com', 10, 2),
+('Gualberto.Alescio@gmail.com', 4, 2);
+
+INSERT INTO Belong1(email, category_id ) VALUES
+('Tatiana.Agnelli@gmail.com','A1')
+('Angelo.Antonini@gmail.com','A6')
+('Gualberto.Alescio@gmail.com','A2')
+('Beatrice.Altoviti@gmail.com','A4');
+
+INSERT INTO Sale_Through(type, email) VALUES
+('Pay In store', 'Tatiana.Agnelli@gmail.com'),
+('Cash On delivery', 'Tatiana.Agnelli@gmail.com'),
+('Pay In store', 'Angelo.Antonini@gmail.com'),
+('Cash On delivery', 'Angelo.Antonini@gmail.com'),
+('Pay In store', 'Gualberto.Alescio@gmail.com'),
+('Pay In store', 'Beatrice.Altoviti@gmail.com');

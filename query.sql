b1b904175bec277eaf4bac67cb75e0dbb6ce1670
Mattia Avanzi <m8.avanzi@gmail.com>
2019-05-23 15:56:47 +0200

    Commentato query.sql

--- query.sql
+++ query.sql
@@ -1,19 +1,8 @@
 -- connect to localProductions db
 \c localProductions
 
-/*
 
-- Visualizzare storico ordini di un utente 
-- Visualizzare un dettaglio ordine (Prodotti, prezzo singolo, prezzo totale ordini, produttore, data) 
-- Visualizzare eventi presenti e futuri in una regione (Quando si tengono, descrizione e i prodotti che contengono)
-
-- STATISTICHE
--- visualizzare statistiche di vendita per un produttore (per ogni prodotto le unita' gia' vendute)
-
-*/
-
-
--- Visualizzare storico ordini (ultimi 15) di un utente
+-- Query to visualize Order History (last 15) of a user
 SELECT o.order_id, o.order_timestamp, o.total_price, m.type FROM Make AS m
 INNER JOIN Orders AS o ON m.order_id = o.order_id
 WHERE customer_email = 'Evelina.Piazza@gmail.com'
@@ -21,8 +10,7 @@ ORDER BY order_id DESC
 LIMIT 15;
 
 
--- Visualizzare un dettaglio ordine (Prodotti, prezzo singolo, produttore)
-
+-- Query to visualize the details of an order given an order ID
 SELECT c.order_id, p.product_code, name, quantity, price AS "Unit Price", business_name FROM Contain as c
 INNER JOIN Make AS m On c.order_id = m.order_id
 INNER JOIN Product AS p ON c.product_code = p.product_code
@@ -31,15 +19,15 @@ WHERE c.order_id = 3
 ORDER BY name ASC;
 
 
--- Visualizzare eventi presenti e futuri in una regione (Quando si tengono, descrizione e i prodotti che contengono)
+-- Query to visualize present and future orders in a region
 SELECT e.event_id, e.name, description, start_date, end_date, location, region_name, p.product_code, p.name FROM Event AS e
 INNER JOIN Promote AS prm ON prm.event_id = e.event_id
 INNER JOIN Product AS p ON p.product_code = prm.product_code
 WHERE end_date >= CURRENT_DATE AND region_name = 'Veneto';
 
 
---STATISTICHE
--- visualizzare statistiche di vendita per un produttore (per ogni prodotto le unita' gia' vendute)
+-- STATISTICS
+-- Query to visualize sell statistics for a given producer (email)
 SELECT c.product_code, SUM(c.quantity) AS "Total Sell" FROM Make AS m
 INNER JOIN Orders AS o ON m.order_id = o.order_id
 INNER JOIN Contain AS c ON m.order_id = c.order_id
